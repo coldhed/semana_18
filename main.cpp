3e5daeb8892fcbda7ec34b9ce440b0ad0f8752f4
coldhed <santiago.rdgz2001@gmail.com>
2022-10-26 17:39:08 -0500

    Se agregó pseudocodigo para homogenizar las clases, funciones, y el programa principal.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,34 @@ using namespace std;
 
 int main() {
     Ordenes ordenes;
+    Restaurante restaurante;
     // AQUI VA EL CICLO
+
+    /*
+    1. Preguntar si es cliente o dueño
+    
+    Cliente:
+    1. Ver el menú. 
+        restaurante.imprimirMenu();
+    2. Para recoger o domicilio?
+    3. Dirección y código postal.
+        3.1 Si es a domicilio, revisar el código postal. Restaurante va a tener una funcion para eso
+
+        3.2 Si no se entrega, ofrecer que sea la orden para recoger
+    4. Orden.
+        Orden* nuevaOrden = new Orden()
+    5. Confirmar pedido -> Dar tiempo estimado
+    6. Agregar orden a ordenes
+        ordenes.agregarOrden(&orden);
+
+    Dueño:
+    1. Ver estado de ordenes o completar órden:
+        Ver ordenes: 
+            ordenes.imprimirOrdenes();
+        Completar órden:
+            ordenes.completarOrden();
+
+    */
 
     ordenes.liberarMemoria();
 }
--- orden.hpp
+++ orden.hpp
@@ -1,5 +1,24 @@
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
 class Orden {
     // Clase con la info de la orden
+    private:
+
+    string direccion;
+    string codigoPostal;
+    unordered_map<string, int> infoOrden;  // se mapea un producto a su cantidad
+
     public:
-        void imprimirOrden() {}
+
+    Orden(string direccion_, string codigoPostal_, unordered_map<string, int> infoOrden_) {
+        direccion = direccion_;
+        codigoPostal = codigoPostal_;
+        infoOrden = infoOrden_;
+    }
+        
+        
+    void imprimirOrden() {}
 };
